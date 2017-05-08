971f4616cdbacbcafa1f38b8fddfeec15a3894a0
vheimpel <vheimpel@gmail.com>
2017-05-08 21:59:00 +0000

    Everything

---
+++ https-library-exercise/http-functions.js
@@ -0,0 +1,25 @@
+
+var https = require('https');
+var string = "";
+
+module.exports = function getHTML (options, callBack) {
+
+      https.get(options, function(buffer) {
+
+        buffer.on('data', function(data) {
+          for (var i = 0; i < data.length; i++) {
+            string = string + data[i] + '\n'
+          }
+          callBack(data);
+        });
+      });
+    }
+
+  function printHTML (html) {
+    console.log(html);
+  }
+  // var requestOptions = {
+  //   host: 'sytantris.github.io',
+  //   path: '/http-examples/step3.html'
+  // };
+
