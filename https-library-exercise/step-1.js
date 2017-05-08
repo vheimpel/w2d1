cabb1d46de2296b6ef4ff2fc419926fdf3c94dc7
vheimpel <vheimpel@gmail.com>
2017-05-08 20:37:50 +0000

    Third for real

--- https-library-exercise/step-1.js
+++ https-library-exercise/step-1.js
@@ -1,5 +1,6 @@
 
 var https = require('https');
+
 function getAndPrintHTMLChunks() {
 
   var requestOptions = {
@@ -8,10 +9,6 @@ function getAndPrintHTMLChunks() {
   };
 
  https.get(requestOptions, function(response) {
-
-  // set encoding of received data to UTF-8
-  response.setEncoding('utf8');
-
   response.on('data', function(data) {
     console.log(data + '\n');
   });
--- https-library-exercise/step-3.js
+++ https-library-exercise/step-3.js
@@ -2,25 +2,25 @@
 var https = require('https');
 var string = "";
 
-function getAndPrintHTML() {
+function getAndPrintHTML(options) {
 
-  var requestOptions = {
-    host: 'sytantris.github.io',
-    path: '/http-examples/step2.html'
-  };
-
-  https.get(requestOptions, function(buffer) {
+  https.get(options, function(buffer) {
 
   buffer.on('data', function(data) {
     for (var i = 0; i < data.length; i++) {
-      string = string + data[i]
+      string = string + data[i] + '\n'
     }
 
-    console.log(string + '\n');
+    console.log(string);
 
   });
 
 });
 }
 
-getAndPrintHTML();
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML(requestOptions);
